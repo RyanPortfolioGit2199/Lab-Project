292412463b20ef3a3c92ac4bd3e51e12d8082a19
RyanPortfolioGit2199 <ryan.allen.2199@gmail.com>
2025-04-20 18:31:32 -0700

    PowerupSpawning&PowerupAbilitySetup

--- Assets/Scripts/BulletMoveForward.cs
+++ Assets/Scripts/BulletMoveForward.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 public class MoveForward : MonoBehaviour
 {
     public float speed = 5.0f;
+    private GameObject Player;
+    PlayerController playerController;
+    private GameObject Enemy;
+    
     // Start is called before the first frame update
     void Start()
     {
+        Player = GameObject.FindWithTag("Player");
+        playerController = GameObject.FindAnyObjectByType<PlayerController>();
         
+        Enemy = GameObject.FindGameObjectWithTag("Asteroid");
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.up * Time.deltaTime * speed);
+        if (playerController.hasPowerUp == true)
+        {
+            Debug.Log("Bullets shot will track an enemy with the powerup equipped");
+        }
+        else
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+        }
+        
     }
 }
